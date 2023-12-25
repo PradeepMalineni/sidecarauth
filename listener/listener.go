e81e0eefbba5e8b53087106df6b227992e533144
PradeepMalineni <pradeep.malineni@gmail.com>
2023-12-25 12:32:52 +0530

    Moved the URL's and Port to config file.

--- listener/listener.go
+++ listener/listener.go
@@ -2,12 +2,71 @@ package listener
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 )
+
+// Configuration is a struct to hold the configuration parameters.
+type Configuration struct {
+	TargetURL  string `json:"targetURL"`
+	ServerPort int    `json:"serverPort"`
+	CertFile   string `json:"certFile"`
+	KeyFile    string `json:"keyFile"`
+}
+
+// ReverseProxy is a struct that implements the http.Handler interface
+// and is used for forwarding requests to another HTTP service.
+type ReverseProxy struct {
+	target *url.URL
+	proxy  *httputil.ReverseProxy
+}
+
+func NewReverseProxy(targetURL string) *ReverseProxy {
+	target, err := url.Parse(targetURL)
+	if err != nil {
+		panic(err)
+	}
+
+	return &ReverseProxy{
+		target: target,
+		proxy:  httputil.NewSingleHostReverseProxy(target),
+	}
+}
+
+//Load the config file
+
+func loadConfig(filename string) (*Configuration, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	config := &Configuration{}
+	err = decoder.Decode(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func (rp *ReverseProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Modify the request if necessary before forwarding
+	// For example, you might want to update headers or paths
+	// r.Header.Add("X-Forwarded-Host", r.Host)
+	log.Printf("Incoming Request: %s %s", r.Method, r.URL.Path)
+
+	// Forward the request to the target service
+	rp.proxy.ServeHTTP(w, r)
+}
 
 func isPortAvailable(port string) bool {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
@@ -35,7 +94,12 @@ func init() {
 
 func Listner() {
 	// Start the HTTP server on port 8080
-	cert, err := tls.LoadX509KeyPair("/Users/siva/sidecarauth/certs/server.crt", "/Users/siva/sidecarauth/certs/server.key")
+	config, err := loadConfig("listener/config.json")
+	if err != nil {
+		log.Fatal("Error loading configuration:", err)
+		os.Exit(1) // Exit the program with error code 1
+	}
+	cert, err := tls.LoadX509KeyPair(config.CertFile, config.KeyFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -44,12 +108,16 @@ func Listner() {
 		Certificates: []tls.Certificate{cert},
 	}
 	addr := ":" + port
+	// Create a reverse proxy that forwards requests to another service
+	targetURL := config.TargetURL // Replace with the target service URL
+	proxy := NewReverseProxy(targetURL)
 
 	// Create an HTTP server with the TLS configuration
 	server := &http.Server{
-		Addr:      addr,
-		Handler:   http.HandlerFunc(handleRequest),
+		Addr: addr,
+		//Handler:   http.HandlerFunc(handleRequest),
 		TLSConfig: tlsConfig,
+		Handler:   proxy,
 	}
 
 	// Start the HTTPS server
@@ -61,7 +129,9 @@ func Listner() {
 
 }
 
+/*
 func handleRequest(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Fprintf(w, "hello\n")
 }
+*/
